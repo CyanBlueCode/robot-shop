018327121b2738baed73f3740424fa1225ab380c
Chase Yang <chase@cyanbluecreative.com>
2021-02-02 20:15:45 -0800

    quicksave

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopePage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       {/* Switch ensures that only first matching route is rendered; provides more control  */}
       <Switch>
+        {/* Note: only the top level component passed into Route will get access to match, history, location props; so to get around this, use withRouter in menu-item.component where the props are actually needed */}
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopePage} />
       </Switch>
     </div>
   );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,7 +5,10 @@ import { withRouter } from 'react-router-dom';
 // match prop comes from react-router-dom
 const MenuItem = ({ title, imgUrl, size, history, linkUrl, match }) => (
   // history.push (to react-router-dom) = /somematchedURL/linkURL
-  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className="background-image"
       style={{
@@ -13,10 +16,12 @@ const MenuItem = ({ title, imgUrl, size, history, linkUrl, match }) => (
       }}
     />
     <div className="content">
+    {/* TODO: figure out why title is coming out as undefined */}
       <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
 );
 
+// withRouter HOC gives a "power-up" to menuItem to give it access to history, location, match props
 export default withRouter(MenuItem);
